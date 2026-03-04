c57e2940978dc5f686b9beeb534c5a3772e027b3
bytekai <fahad.alhatshami@hotmaik.com>
2026-03-04 03:50:49 +0100

    updates

--- packages/e2e/src/sandbox.ts
+++ packages/e2e/src/sandbox.ts
@@ -13,7 +13,7 @@ import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-export const VYFT_CLI = join(__dirname, "../../vyft/dist/cli/index.js");
+export const VYFT_CLI = join(__dirname, "../../vyft/dist/cli.js");
 const PACKAGES_DIR = join(__dirname, "../..");
 
 /**
